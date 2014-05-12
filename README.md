96574bc44e7881bb5f4e07bd0068a54fabed6052
Bertrand Roussel <broussel@sierrawireless.com>
2014-05-12 11:29:53 +0200

    CSV + multiple devices support

--- README.md
+++ README.md
@@ -13,4 +13,8 @@ There's a lot to be done. Right now, it just supports the following.
 This is an excellent guide on SCPI for those who want to build/read/add more.
 http://www.phas.ubc.ca/~phys409/HPManuals/HP34401A_RemoteGuide.pdf
 
+Can control multiple devices using "ports" argument.
+
+Also, results can be exported to CSV, which can be imported in a spreadsheet, or used in gnuplot to draw measurements live for instance.
+
 Error handling is completlely absent, the only reason I even wrote this and uploaded this is because I couldn't find something existing that would let me just take a couple of measurements off the multimeter.
--- multimeter.py
+++ multimeter.py
@@ -1,10 +1,13 @@
-import serial, time
+#!/usr/bin/env python
 
+import serial, time
+import sys
+import argparse
 
 class Multimeter(object):
     def __init__(self,port='/dev/ttyUSB0'):
         ser = serial.Serial()
-        ser.port = '/dev/ttyUSB0'
+        ser.port = port
         ser.baudrate = 9600
         ser.bytesize = serial.EIGHTBITS
         ser.parity = serial.PARITY_NONE
@@ -29,7 +32,6 @@ class Multimeter(object):
         self.writeCommand('*IDN?')
         resp = self.ser.readline()
         self.id = resp
-        #print resp
         return resp
 
     def reset(self):
@@ -55,31 +57,127 @@ class Multimeter(object):
             self.writeCommand('DISPLAY 0')
 
 
-    def setupForRemote(self):
+    def setupForRemote(self, reset=True):
         self.connect()
         self.getId()
         self.clear()
-        self.reset()
-        self.clear()
+
+        if reset:
+            self.reset()
+            self.clear()
+
         self.display(True)
         self.setSystem('REM')
-        
-        
+
+
     def measure(self,what="VOLT:DC",range=10.0,resolution=0.003):
         wcmd = 'MEAS:'+what+"? %f,%f"%(range,resolution,)
+        wcmd = 'MEAS:'+what+"? %f"%(range)
         self.writeCommand(wcmd)
+        return self.readResult()
+
+    def start(self,what="VOLT:DC",range=10.0,resolution=0.003,samples=1):
+        wcmd = 'CONF:'+what+" %f"%(range)
+        self.writeCommand(wcmd)
+        self.writeCommand("SAMPLe:COUNt %f" % (samples))
+        self.writeCommand("TRIG:SOUR IMM")
+        #self.writeCommand("INIT")
+        #self.writeCommand("FETC?")
+
+    def read(self):
+        self.writeCommand("READ?")
+        return self.readResult()
+
+    def readResult(self):
         resp = self.ser.readline()
+        if not resp:
+            self.readError()
+            return -1
         return float(resp)
 
     def readError(self):
-        a.writeCommand('SYST:ERROR?')
-        resp = a.ser.readline()
+        self.writeCommand('SYST:ERROR?')
+        resp = self.ser.readline()
         print resp
         return resp
-        
+
+    def doCommand(self,what):
+        self.writeCommand(what)
+        resp = self.ser.readline()
+        if resp:
+            print resp
+        return resp
+
 if __name__ == "__main__":
-    a = Multimeter()
-    a.setupForRemote()
-    print a.id
-    voltage = a.measure()
-    print voltage
+    csv = None
+    devices = {}
+
+    parser = argparse.ArgumentParser(description='SCPI Interface')
+    parser.add_argument('-p','--ports',
+        dest="ports",
+        action='append',
+        default=[],
+        help="Ports to read from, can read on multiple ports simultaneously.")
+    parser.add_argument("-o","--output",
+        dest="output",
+        default=None,
+        help="CSV output")
+
+    args = parser.parse_args()
+    ports = args.ports
+
+    if len(ports) == 0:
+        ports = ["/dev/ttyUSB0"]
+
+    if args.output:
+        csv = open(args.output,"w+")
+        csv.write("Nb,Time (s)")
+        for i in range(1, len(ports)):
+            csv.write(",Value %d (mA)" % (i))
+        csv.write("\n")
+
+    for port in ports:
+        device = Multimeter(port)
+        device.setupForRemote(True) #False)
+        device.doCommand("SYSTem:BEEPer:STATe OFF")
+        device.doCommand("TRIGger:DELay:AUTO ON")
+
+        devices[port] = device
+
+    i = 1
+    tsStart = time.time()
+
+    for port in ports:
+        devices[port].start("CURR:DC", range=0.5, samples=1)
+
+    try:
+        while 1:
+            #amp = a.measure("CURR:DC", 0.1)
+            #amp = a.ser.readline()
+            readings = {}
+            for port in ports:
+                readings[port] = devices[port].read()
+
+            tsNow = time.time()
+            tsOffset = tsNow - tsStart
+
+            sys.stdout.write("%d:" % (tsOffset))
+            for port in ports:
+                sys.stdout.write(" %f" % readings[port])
+            sys.stdout.write("\n")
+
+            if csv:
+                line = "%d,%f\n" % (i, tsOffset)
+                for port in ports:
+                    line += ",%f" % (readings[port] * 1000)
+                line += "\n"
+
+                #line = line.replace(".",",")
+                csv.write(line)
+                csv.flush()
+
+            i += 1
+
+    except (KeyboardInterrupt, SystemExit):
+        csv.close()
+        exit(0)
